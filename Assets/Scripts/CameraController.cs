b0a68445f9e9a102831f07c3d91db2ff94d17df7
Six Jonathan <Six-jonathan@orange.fr>
2021-09-09 13:49:52 +0200

    [Fix] Fix local -> global ccam rot

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -42,7 +42,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mBaseCameraAngle = mCam.transform.rotation;
+        mBaseCameraAngle = mCam.transform.localRotation;
         mFreeModeOrthoSize = mCam.orthographicSize;
     }
 
